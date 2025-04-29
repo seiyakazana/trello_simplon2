c79638be3c07d44e078295d8c701781095c517dd
CVincent27 <cardon.vincent@hotmail.fr>
2025-04-29 16:57:52 +0200

    premier test cards

---
+++ cards.py
@@ -0,0 +1,8 @@
+list_cards1 = {'titre1' : 'content_cards1', 'titre2' : 'content_cards2'}
+
+for key, values in list_cards1.items():
+    cards = {key : values}
+    board_list = [cards]
+    print(board_list)
+
+
