eac19c4562be75e6782e9bd6869d90ec42d6e4c3
elias <jean-pierre.elias@hotmail.fr>
2025-04-30 10:37:03 +0200

    creation feature list

--- List.py
+++ List.py
@@ -1,21 +1,45 @@
- #Boards (création, suppression, listing)
-home = {}
+# Lists (ajout, suppression, listing au sein d’un board)
+from config import data
 
-def create_boards():
-    name = input("name of the board you want to create")
-    home = {name:{}}
-    return
+def ajout_list(board,list_name):
+    data[board].append({'name':list_name,'lists':[]})
+
+def sup_list(board,list_name):
+    data[board].remove({'name':list_name,'lists':[]})
+
+def listing(board):
+    for i in range(len(data[board])):
+        print(data[board][i]["name"])
+
+#TEST
+ajout_list("boards","PROJET Y")
+sup_list("boards","PROJET Y")
+ajout_list("boards","PROJET Z")
+listing("boards")
+print(data['boards'])
 
 
-def sup_boards():
-    name = input("name of the board you want to delete")
-    del home[name]
-    return 
 
-create_boards()
-print(home)
-sup_boards()
-print(home)
+# for list in data["boards"]:
+#     print(list)
+
+# for board in data:
+#     for i in range(len(data[board])):
+#         print(data[board][i])
+
+# for item in data:
+#             data_item = data[item]
+#             for i in range(len(data[item])):
+
+# def sup_boards():
+#     name = input("name of the board you want to delete")
+#     del home[name]
+#     return 
+
+# create_boards()
+# print(home)
+# sup_boards()
+# print(home)
 
 
 # Le projet suiavnt consiste à développer un mini-Trello en ligne de commande, entièrement basé sur des fonctions et sans recours à la POO, afin de familiariser un groupe de trois apprenants Python avec un véritable workflow Git (Git Flow). Les données seront stockées dans un unique fichier JSON (data.json) : on y gère des « boards », chaque board contenant plusieurs « lists » (colonnes) et chaque list une collection de « cards » (éléments). Les apprenants travailleront en parallèle sur trois modules distincts :
