160dbf1750324b1860594611e433a7f6efab3f74
CVincent27 <cardon.vincent@hotmail.fr>
2025-04-30 09:25:28 +0200

    add data json

--- cards.py
+++ cards.py
@@ -1,8 +1,5 @@
-list_cards1 = {'titre1' : 'content_cards1', 'titre2' : 'content_cards2'}
+from config import data
 
-for key, values in list_cards1.items():
-    cards = {key : values}
-    board_list = [cards]
-    print(board_list)
+print(data)
 
 
---
+++ config.py
@@ -0,0 +1,5 @@
+import json
+
+
+with open("src/data.json", "r") as f:
+    data = json.load(f)
